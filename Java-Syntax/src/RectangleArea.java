3bb1d98f45f4c1c21822cb2e9113c77acdc5e1e9
aanguelov <aanguelov@gmail.com>
2015-08-31 12:25:47 +0300

    Initial commit

---
+++ Java-Syntax/src/RectangleArea.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sideA = scanner.nextInt();
+        int sideB = scanner.nextInt();
+        int area = sideA*sideB;
+        System.out.println(area);
+    }
+}
